1bb525aff6d5c2db591f5cb5ca7162bace6eca48
Joseph Keable <keablejoseph@gmail.com>
2021-07-27 11:07:20 +0100

    finishes video content

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
+const app = express();
+const members = require("./Members");
+
+//Init Middleware
+// app.use(logger);
+
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Homepage Route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members,
+  })
+);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Members API route
+app.use("/api/members", require("./routes/api/members"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
